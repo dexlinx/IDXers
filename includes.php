a4775cd1860c589433991cea0a8031c2c2332996
Tony Rappa <dexlinx@gmail.com>
2017-05-31 09:49:02 -0700

    Create includes.php

---
+++ includes.php
@@ -0,0 +1,9 @@
+<?php
+//Site URL
+$SITE_URL = "http://learn.idxbroker.com";
+$VOTE_IMG = "/wp-content/plugins/idxers/vote.png";
+$VOTED_IMG = "/wp-content/plugins/idxers/voted.png";
+$PAGE_PATH = $_SERVER['REQUEST_URI'];
+$VOTE_PLUGIN_DIR = '/wp-content/plugins/idxers/';
+$FB_APP_ID = '1116440061761832';
+?>
